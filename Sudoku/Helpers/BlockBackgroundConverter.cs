236914bbc24cf90ad0db7b45b1a041671d73d2c1
AnjaAcic <acicanja@gmail.com>
2025-11-14 21:56:00 +0100

    fix game view

---
+++ Sudoku/Helpers/BlockBackgroundConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Sudoku.Helpers
+{
+    public class BlockBackgroundConverter : IValueConverter
+    {
+        private static readonly Brush[] BlockColors = new Brush[]
+        {
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F4E5F7")), // svetlo ljubičasta
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E5F0FA")), // svetlo plava
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E5FAF0")), // svetlo zelena
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF5E5")), // svetlo narandžasta
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FDE5F0")), // svetlo roze
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E5FAF9")), // svetlo tirkizna
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F5F5E5")), // svetlo žuta
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EDE5FA")), // svetlo ljubičasta 2
+            new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F0FAE5"))  // svetlo zelena 2
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                int row = index / 9;
+                int col = index % 9;
+
+                int blockRow = row / 3;
+                int blockCol = col / 3;
+                int blockIndex = blockRow * 3 + blockCol;
+
+                return BlockColors[blockIndex];
+            }
+
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sudoku/Helpers/SudokuBorderThicknessConverter.cs
+++ Sudoku/Helpers/SudokuBorderThicknessConverter.cs
@@ -15,15 +15,19 @@ namespace Sudoku.Helpers
                 int row = cell.Row;
                 int col = cell.Col;
 
-                double left = (col % 3 == 0) ? 2 : 0.5;
-                double top = (row % 3 == 0) ? 2 : 0.5;
-                double right = ((col + 1) % 3 == 0) ? 2 : 0.5;
-                double bottom = ((row + 1) % 3 == 0) ? 2 : 0.5;
+                double thin = 1;    
+                double thick = 2;   
+
+                double left = (col % 3 == 0) ? thick : thin;
+                double top = (row % 3 == 0) ? thick : thin;
+                double right = ((col + 1) % 3 == 0) ? thick : thin;
+                double bottom = ((row + 1) % 3 == 0) ? thick : thin;
 
                 return new Thickness(left, top, right, bottom);
             }
-            return new Thickness(0.5);
+            return new Thickness(1);
         }
+
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotImplementedException();
